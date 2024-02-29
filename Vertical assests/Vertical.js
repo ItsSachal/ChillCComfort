dd2a59cd3e8dbf4c807727da5f8fdd9494a8b6da
SachalMurtaza <sachalmurtaza145@gmail.com>
2024-02-29 08:39:16 -0800

    My Folder

---
+++ Vertical assests/Vertical.js
@@ -0,0 +1,16 @@
+let allproducts=document.getElementById("verticalproducts")
+let products=[
+    {name:"Vertical AC",image:"./Vertical/1.png",pprice:"1000000"},
+    {name:"Vertical AC",image:"./Vertical/2.png",pprice:"1000000"},
+];
+for (var i = 0; i < products.length; i++) {
+    allproducts.innerHTML += `<div class="col-lg-3 col-md-4 col-sm-6">
+      <div class="card"  style="width:100%; z-index:20;">
+      <img src="${products[i].image}" class="card-img-top" height="200" width="100%" alt="...">
+      <div class="card-body">
+        <h5 class="card-title text-warning">${products[i].name}</h5>
+        <p class="card-text text-success">${products[i].pprice}</p>
+        <a href="#" class="btn btn-primary">Add To Cart</a>
+      </div>
+    </div>`;
+  }
